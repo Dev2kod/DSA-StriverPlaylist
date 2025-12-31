47ba07e351044ca4325e67a3b1c683d624498bb5
Dev <113640546+Dev2kod@users.noreply.github.com>
2025-12-31 10:56:54 +0000

    updating 31st december

---
+++ dsa2/Array/Medium/2sum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(vector<int> &arr,int target){
+
+    int n = arr.size();
+    int i =0;
+    int j =0;
+
+    while(j<n){
+        
+    }
+
+    return 0;
+}
+
+
+int main(){
+
+vector<int> arr={2,6,5,8,11};
+int target = 14;
+
+sum(arr,target);
+
+    return 0;
+}
